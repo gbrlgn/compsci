b04c8cda5c32e30e6528a83c1dddd04813c575fd
Gabriel Gian <gabrielgian@protonmail.com>
2022-02-17 16:23:03 -0300

    Add files from Alura's Clojure course 2

---
+++ courses/alura/programming/clojure/clojure_02/loja/src/loja/aula1.clj
@@ -0,0 +1,13 @@
+(ns loja.aula1)
+
+(def vet ["Gabriel" "Deleuze" "Nietzsche" "Leibniz"])
+
+(def meu-map
+  [func seq]
+  (let [prim (first seq)]
+    (if prim
+      (do
+        (func prim)
+        (meu-map func (rest seq))))))
+
+(meu-map println vet)
---
+++ courses/alura/programming/clojure/clojure_02/loja/src/loja/core.clj
@@ -0,0 +1,7 @@
+(ns loja.core)
+
+;; ["Gabriel" "Deleuze" "Nietzsche" "Leibniz"]
+;; {"Gabriel" 23, "Guilherme" 37}
+;; '(1 2 3 4 5)
+;; [[0 1]]
+;; #{"set" "conjunto"}
