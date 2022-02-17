dac5f131ba288d98428414f09b4901853989f999
Gabriel Gian <gabrielgian@protonmail.com>
2022-02-17 17:06:05 -0300

    Add loop variation

--- courses/alura/programming/clojure/clojure_02/loja/src/loja/aula2.clj
+++ courses/alura/programming/clojure/clojure_02/loja/src/loja/aula2.clj
@@ -15,3 +15,15 @@
      total-agora)))
 
 (println (conta 0 [1 2 3 4]))
+
+
+;; Uso de loop (caso haja algum código a ser executado
+;; antes (ou fora) da execução
+(defn conta
+  [elementos]
+  (println "Antes da recursão!")
+  (loop [total-agora 0
+         elementos-rest elementos]
+    (if (seq elementos-rest)
+      (recur (inc total-agora) (next elementos-rest))
+      total-agora)))
