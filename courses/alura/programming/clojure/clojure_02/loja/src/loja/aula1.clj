0b097cd733ea1cb2321ca8f9580a49233e4759f4
Gabriel Gian <gabrielgian@protonmail.com>
2022-02-17 16:34:53 -0300

    Make tail recursion using recur

--- courses/alura/programming/clojure/clojure_02/loja/src/loja/aula1.clj
+++ courses/alura/programming/clojure/clojure_02/loja/src/loja/aula1.clj
@@ -5,9 +5,24 @@
 (def meu-map
   [func seq]
   (let [prim (first seq)]
-    (if (not(nil? prim))
+    (if (not (nil? prim))
       (do
         (func prim)
         (meu-map func (rest seq))))))
 
 (meu-map println vet)
+
+;; Causa um stack overflow
+(meu-map println (range 1000000))
+
+;; recur faz com que a recursão não seja custosa
+;; armazenando na pilha de execução (tail recursion)
+(def meu-map
+  [func seq]
+  (let [prim (first seq)]
+    (if (not (nil? prim))
+      (do
+        (func prim)
+        (recur (meu-map func (rest seq)))))))
+
+(meu-map println (range 100000))
