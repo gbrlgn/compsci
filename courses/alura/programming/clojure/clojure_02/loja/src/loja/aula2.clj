8df4d72fb152ebf9cc1304b9ba2855fdfed4c2b8
Gabriel Gian <gabrielgian@protonmail.com>
2022-02-17 16:57:09 -0300

    Edit function to use different arities

--- courses/alura/programming/clojure/clojure_02/loja/src/loja/aula2.clj
+++ courses/alura/programming/clojure/clojure_02/loja/src/loja/aula2.clj
@@ -6,7 +6,12 @@
   (recur (inc total-agora(rest elementos))))
 
 (defn conta
-  [total-agora elementos]
-  (if (seq elementos)
-    (recur (inc total-agora (next elementos)))
-    total-agora))
+  ([elementos]
+   (conta 0 elementos))
+
+  ([total-agora elementos]
+   (if (seq elementos)
+     (recur (inc total-agora (next elementos)))
+     total-agora)))
+
+(println (conta 0 [1 2 3 4]))
