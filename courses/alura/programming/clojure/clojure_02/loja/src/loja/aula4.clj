528b3ba45d83027007f6985180baffab362ab15a
Gabriel Gian <gabrielgian@protonmail.com>
2022-02-18 12:12:51 -0300

    Add sort-by methods

---
+++ courses/alura/programming/clojure/clojure_02/loja/src/loja/aula4.clj
@@ -0,0 +1,16 @@
+(ns loja.aula4
+  :require [loja.db :as l.db
+            loja.logic :as l.logic])
+
+(let [pedidos (l.db/todos-os-pedidos)
+      resumo (l.logic/resumo-por-usuario (pedidos))]
+  (println "Resumo" resumo)
+  (println "Ordenado" (sort-by :preco-total resumo))
+  (println "Ordenado ao contrário" (reverse (sort-by :preco-total resumo)))
+  (println "Ordenado por ID" (sort-by :usuario-id resumo))
+  (println (get-in pedidos [0 :itens :mochila :quantidade])))
+
+(defn resumo-ord-usuario
+  [pedidos]
+  (->> pedidos
+       (sort-by :preco-total)))
