7ec2f7d0fc0526bd1d02ac6c21ad8afae4695564
Gabriel Gian <gabrielgian@protonmail.com>
2022-02-18 10:48:29 -0300

    Use group-by and keywords to map through a database of items

--- courses/alura/programming/clojure/clojure_02/loja/src/loja/aula3.clj
+++ courses/alura/programming/clojure/clojure_02/loja/src/loja/aula3.clj
@@ -12,8 +12,29 @@
 
 (println (map count (vals (group-by :usuario (l.db/todos-os-pedidos)))))
 
+(defn total-do-item
+  [[item-id detalhes]]
+  (* (get detalhes :quantidade 0) (get detalhes :preco-unitario 0)))
+
+(defn total-do-pedido
+  [pedido]
+  (reduce + (map total-do-item pedido)))
+
+(defn total-pedidos
+  [pedidos]
+  (->> pedidos
+       (map :itens)
+       (map total-do-pedido)
+       (reduce +)))
+
+(defn total-usuario
+  [[usuario pedidos]]
+  {:usuario-id usuario
+   :total-de-pedidos (count pedidos)
+   :preco-total (total-pedidos pedidos)})
+
 (->> l.db/todos-os-pedidos
      (group-by :usuario)
-     vals
+     (map total-usuario)
      (map count)
      println)
