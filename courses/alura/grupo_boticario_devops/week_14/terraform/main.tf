4a670cfdbd95f23ae55494eaac2cb33575ee9825
Gabriel Gian <gabrielgian@protonmail.com>
2022-02-18 14:05:57 -0300

    Add Terraform main config

---
+++ courses/alura/grupo_boticario_devops/week_14/terraform/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    version = "~> 2.0"
+    region = "us-east-1"
+}
+
+resource "aws_instance" "dev" {
+    count = 3
+    ami = "ami-04505e74c0741db8d"
+    instance_type = "t2.micro"
+    key_name = "terraform-aws"
+    tags = {
+        Name = "1"
+    }
+}
