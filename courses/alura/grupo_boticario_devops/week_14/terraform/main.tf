68912e5afb23236b4ab42eebf9f9058290555699
Gabriel Gian <gabrielgian@protonmail.com>
2022-02-18 16:56:25 -0300

    Add different AWS regions to main.tf

--- courses/alura/grupo_boticario_devops/week_14/terraform/main.tf
+++ courses/alura/grupo_boticario_devops/week_14/terraform/main.tf
@@ -1,5 +1,11 @@
 provider "aws" {
+    alias = "us-east-1"
     region = "us-east-1"
+}
+
+provider "aws" {
+    alias = "us-east-2"
+    region = "us-east-2"
 }
 
 resource "aws_instance" "dev" {
@@ -36,6 +42,18 @@ resource "aws_instance" "dev5" {
     }
 
     vpc_security_group_ids = ["${aws_security_group.acesso_ssh.id}"]
+}
+
+resource "aws_instance" "dev6" {
+    provider = "aws.us-east-2"
+    ami = "ami-0fb653ca2d3203ac1"
+    instance_type = "t2.micro"
+    key_name = "terraform-aws"
+    tags = {
+        Name = "dev6"
+    }
+
+    vpc_security_group_ids = ["${aws_security_group.acesso_ssh_us_east_2.id}"]
 }
 
 resource "aws_s3_bucket" "dev4" {
--- courses/alura/grupo_boticario_devops/week_14/terraform/security_group.tf
+++ courses/alura/grupo_boticario_devops/week_14/terraform/security_group.tf
@@ -17,3 +17,24 @@ resource "aws_security_group" "acesso_ssh" {
         }
     ]
 }
+
+resource "aws_security_group" "acesso_ssh_us_east_2" {
+    provider = "aws.us-east-2"
+    name = "acesso_ssh"
+    description = "acesso_ssh"
+
+    ingress = [
+        {
+            description = "acesso_ssh"
+            from_port = 22
+            to_port = 22
+            protocol = "tcp"
+            
+            cidr_blocks = ["152.244.252.0/24"]
+            ipv6_cidr_blocks = []
+            prefix_list_ids = []
+            security_groups = []
+            self = true
+        }
+    ]
+}
