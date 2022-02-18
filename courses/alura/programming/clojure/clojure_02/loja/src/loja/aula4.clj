5e9bfda666017daf2c6d2a3f0ad98187b9a553c3
Gabriel Gian <gabrielgian@protonmail.com>
2022-02-18 12:20:58 -0300

    Add get variations

--- courses/alura/programming/clojure/clojure_02/loja/src/loja/aula4.clj
+++ courses/alura/programming/clojure/clojure_02/loja/src/loja/aula4.clj
@@ -3,7 +3,7 @@
             loja.logic :as l.logic])
 
 (let [pedidos (l.db/todos-os-pedidos)
-      resumo (l.logic/resumo-por-usuario (pedidos))]
+      resumo (resumo-ord-usuario (pedidos))]
   (println "Resumo" resumo)
   (println "Ordenado" (sort-by :preco-total resumo))
   (println "Ordenado ao contrário" (reverse (sort-by :preco-total resumo)))
@@ -13,4 +13,19 @@
 (defn resumo-ord-usuario
   [pedidos]
   (->> pedidos
+       l.logic/resumo-por-usuario
        (sort-by :preco-total)))
+
+(let [pedidos (l.db/todos-os-pedidos)
+      resumo (resumo-ord-usuario pedidos)]
+  (println "Resumo" resumo)
+  (println "Primeiro" (first resumo))
+  (println "Segundo" (second resumo))
+  (println "Resto" (rest resumo))
+  (println "Classe" (class resumo))
+  (println "Enésimo" (nth resumo 1))
+  (println "Take" (take 2 resumo)))
+
+(defn top-2
+  [pedidos]
+  (take 2 pedidos))
