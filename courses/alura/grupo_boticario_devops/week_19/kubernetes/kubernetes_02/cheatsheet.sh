2e15f4303d15f1e6600b05956fc830c216ba9e11
Gabriel Gian <gabrielgian@protonmail.com>
2022-02-21 10:14:55 -0300

    Add files and create first ReplicaSet

---
+++ courses/alura/grupo_boticario_devops/week_19/kubernetes/kubernetes_02/cheatsheet.sh
@@ -0,0 +1,35 @@
+# Cria um pod do Kubernetes.
+kubectl run nginx-pod --image=nginx:latest
+
+# Listar pods.
+kubectl get pods
+
+# Listar pods em tempo real.
+kubectl get pods --watch
+
+# Listar informações de um pod.
+kubectl describe pod nginx-pod
+
+# Editar configurações do pod.
+kubectl edit pos nginx-pod
+
+# Aplicar um pod de maneira declarativa.
+kubectl apply -f ./primeiro-pod.yml
+
+# Deletar um pod.
+kubectl delete pod nginx-pod
+
+# Executar um comando em um pod em execução.
+kubectl exec -it portal-noticias -- bash
+
+# Listar pods com mais informações.
+kubectl get pods -o wide
+
+# Listar serviços de pods.
+kubectl get svc
+
+# Deletar todos os pods.
+kubectl delete pods --all
+
+# No Linux, o INTERNAL-IP é utilizado para o
+# mapeamento de IPs do cluster.
