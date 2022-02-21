39e63121dd357fd7525cc325c47d2778d1e40d32
Gabriel Gian <gabrielgian@protonmail.com>
2022-02-21 18:01:42 -0300

    Edit scripts for fixing a Vagrant error

--- courses/alura/grupo_boticario_devops/week_24/jenkins/scripts/jenkins.sh
+++ courses/alura/grupo_boticario_devops/week_24/jenkins/scripts/jenkins.sh
@@ -1,4 +1,10 @@
-wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | apt-key add - 
-sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list' 
-apt-get update 
-apt-get install jenkins
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | sudo tee \
+/usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+https://pkg.jenkins.io/debian binary/ | sudo tee \
+/etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install fontconfig openjdk-11-jre
+sudo apt-get install jenkins
