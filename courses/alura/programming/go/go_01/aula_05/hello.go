f3e577f7187d0414a713973c4b21d9487026e057
Gabriel Gian <gabrielgian@protonmail.com>
2022-02-22 11:17:25 -0300

    Add files from classes 5, 6 and 7

---
+++ courses/alura/programming/go/go_01/aula_05/hello.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+    nome := "Douglas"
+    versao := 1.1
+    fmt.Println("Olá, sr.", nome)
+    fmt.Println("Este programa está na versão", versao)
+
+    fmt.Println("1- Iniciar Monitoramento")
+    fmt.Println("2- Exibir Logs")
+    fmt.Println("0- Sair do Programa")
+
+    var comando int
+    fmt.Scan(&comando)
+    fmt.Println("O comando escolhido foi", comando)
+
+    if comando == 1 {
+        fmt.Println("Monitorando...")
+    } else if comando == 2 {
+        fmt.Println("Exibindo Logs...")
+    } else if comando == 0 {
+        fmt.Println("Saindo do programa...")
+    } else {
+        fmt.Println("Não conheço este comando")
+    }
+}
