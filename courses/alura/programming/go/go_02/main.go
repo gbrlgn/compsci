013400f4afbe6e0f6cc7f64e3a023b043175f8ce
Gabriel Gian <gabrielgian@protonmail.com>
2022-02-22 20:19:33 -0300

    Add first file from Go OO course

---
+++ courses/alura/programming/go/go_02/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
+
+func main() {
+	contaDoGuilherme := ContaCorrente{titular: "Guilherme",
+		numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
+
+	contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 200}
+
+	fmt.Println(contaDoGuilherme)
+	fmt.Println(contaDaBruna)
+
+	contaDaSilvia := ContaCorrente{}
+    contaDaSilvia.titular = "Silvia"
+    contaDaSilvia.saldo = 500
+
+    fmt.Println(contaDaSilvia.saldo)
+    fmt.Println(contaDaSilvia.Sacar(400))
+    fmt.Println(contaDaSilvia.saldo)
+}
+
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+    podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+    if podeSacar {
+            c.saldo -= valorDoSaque
+            return "Saque realizado com sucesso"
+    } else {
+        return "Saldo insuficiente"
+    }
+}
