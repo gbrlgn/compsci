8d1d841e7d9f661ac7cd8358f3bca1fa532800bb
Gabriel Gian <gabrielgian@protonmail.com>
2022-02-22 13:54:58 -0300

    Finish Go course

--- courses/alura/programming/go/go_01/aula_10/hello.go
+++ courses/alura/programming/go/go_01/aula_10/hello.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+    "bufio"
     "fmt"
+    "io"
+    "io/ioutil"
     "net/http"
     "os"
-	"time"
-	"io/ioutil"
+    "strconv"
+    "strings"
+    "time"
 )
 
 const monitoramentos = 3
@@ -23,6 +27,7 @@ func main() {
 			iniciarMonitoramento()
 		case 2:
 			fmt.Println("Exibindo Logs...")
+			imprimeLogs()
 		case 0:
 			fmt.Println("Saindo do programa")
 			os.Exit(0)
@@ -56,21 +61,28 @@ func leComando() int {
 }
 
 func leSitesDoArquivo() []string {
-
     var sites []string
-    arquivo, err := ioutil.ReadFile("sites.txt")
+    arquivo, err := os.Open("sites.txt")
 
 	if err != nil {
         fmt.Println("Ocorreu um erro: ", err)
     }
-	
-    fmt.Println(string(arquivo))
 
+	leitor := bufio.NewReader(arquivo)
+	
+    for {
+        linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+        sites = append(sites, linha)
+        if err == io.EOF {
+			break
+        }
+    }
+	arquivo.Close()
     return sites
 }
 
 func testaSite(site string) {
-
     resp, err := http.Get(site)
 	
 	if err != nil {
@@ -79,8 +91,10 @@ func testaSite(site string) {
 
     if resp.StatusCode == 200 {
         fmt.Println("Site:", site, "foi carregado com sucesso!")
+        registraLog(site, true)
     } else {
         fmt.Println("Site:", site, "está com problemas. Status Code: ", resp.StatusCode)
+        registraLog(site, false)
     }
 }
 
@@ -91,7 +105,7 @@ func iniciarMonitoramento() {
 
 	for i := 0; i < monitoramentos; i++ {
 		for i, site := range sites {
-			fmt.Println("Testando site", i, ":", site)
+			fmt.Println("Testando site", i, ": ", site)
 			testaSite(site)
 		}
 		time.Sleep(delay * time.Minute)
@@ -99,4 +113,26 @@ func iniciarMonitoramento() {
 	}
 
 	fmt.Println("")
+}
+
+func registraLog(site string, status bool) {
+    arquivo, err := os.OpenFile("log.txt", os.O_CREATE | os.O_RDWR | os.O_APPEND, 0666)
+
+    if err != nil {
+        fmt.Println("Ocorreu um erro: ", err)
+    }
+
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + site + " - online: " + strconv.FormatBool(status) + "\n")
+
+    fmt.Println(arquivo)
+}
+
+func imprimeLogs() {
+    arquivo, err := ioutil.ReadFile("log.txt")
+
+    if err != nil {
+        fmt.Println("Ocorreu um erro: ", err)
+    }
+
+    fmt.Println(string(arquivo))
 }
