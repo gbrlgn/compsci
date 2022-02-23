446d756b5105d1bbde0f1ef84f7964e2a19ccf76
Gabriel Gian <gabrielgian@protonmail.com>
2022-02-23 12:19:35 -0300

    Fix attribute visibilities

---
+++ courses/alura/programming/go/go_02/aula_06/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"../contas"
+)
+
+func main() {
+	contaExemplo := contas.ContaCorrente{}
+	contaExemplo.Depositar(100)
+
+	fmt.Println(contaExemplo.ObterSaldo())
+}
