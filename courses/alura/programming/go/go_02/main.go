b6f2a6131035316af27d02787b0a2ae488ea4f23
Gabriel Gian <gabrielgian@protonmail.com>
2022-02-23 10:01:11 -0300

    Add multiple returns to deposit function

--- courses/alura/programming/go/go_02/main.go
+++ courses/alura/programming/go/go_02/main.go
@@ -9,6 +9,25 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "Depósito realizado com sucesso", c.saldo
+	} else {
+		return "O valor do depósito é menor que zero", c.saldo
+	}
+}
+
 func main() {
 	contaDoGuilherme := ContaCorrente{titular: "Guilherme",
 		numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
@@ -19,20 +38,13 @@ func main() {
 	fmt.Println(contaDaBruna)
 
 	contaDaSilvia := ContaCorrente{}
-    contaDaSilvia.titular = "Silvia"
-    contaDaSilvia.saldo = 500
+	contaDaSilvia.titular = "Silvia"
+	contaDaSilvia.saldo = 500
 
-    fmt.Println(contaDaSilvia.saldo)
-    fmt.Println(contaDaSilvia.Sacar(400))
-    fmt.Println(contaDaSilvia.saldo)
-}
+	fmt.Println(contaDaSilvia.saldo)
+	fmt.Println(contaDaSilvia.Sacar(400))
+	fmt.Println(contaDaSilvia.saldo)
 
-func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-    podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
-    if podeSacar {
-            c.saldo -= valorDoSaque
-            return "Saque realizado com sucesso"
-    } else {
-        return "Saldo insuficiente"
-    }
+	contaDaSilvia.Depositar(500)
+	fmt.Println(contaDaSilvia.saldo)
 }
