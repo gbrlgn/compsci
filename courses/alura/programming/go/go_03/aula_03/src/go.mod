c325970d65fe9ac8ec657daf345020260ed5aeb2
Gabriel Gian <gabrielgian@protonmail.com>
2022-02-24 11:44:10 -0300

    Add files for connecting to database and creating a template server

---
+++ courses/alura/programming/go/go_03/aula_03/src/go.mod
@@ -0,0 +1,5 @@
+module alura.com.br/aula_03
+
+go 1.18
+
+require github.com/lib/pq v1.10.4
--- courses/alura/programming/go/go_03/aula_03/src/main.go
+++ courses/alura/programming/go/go_03/aula_03/src/main.go
@@ -1,27 +1,65 @@
 package main
 
 import (
+	"crypto/md5"
+	"database/sql"
+	"encoding/hex"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
 type Produto struct {
+	Id, Quantidade  int
 	Nome, Descricao string
 	Preco           float64
-	Quantidade      int
 }
 
 var tmpl = template.Must(template.ParseGlob("templates/*.html"))
 
+func conectaBD() *sql.DB {
+	pass := md5.Sum([]byte("admin"))
+	hash := hex.EncodeToString(pass[:])
+
+	conexao := "user=admin dbname=alura password=" + hash + " host=localhost sslmode=disable"
+
+	db, err := sql.Open("postgres", conexao)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
-		{"Tenis", "Confortável", 89, 3},
-		{"Fone", "Muito bom", 59, 2},
-		{"Produto novo", "Muito legal", 1.99, 1},
+	db := conectaBD()
+
+	selectTudo, err := db.Query("SELECT * FROM produtos")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Produto{}
+	produtos := []Produto{}
+
+	for selectTudo.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err := selectTudo.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Id, p.Nome, p.Descricao, p.Preco, p.Quantidade = id, nome, descricao, preco, quantidade
+
+		produtos = append(produtos, p)
 	}
 
 	tmpl.ExecuteTemplate(w, "Index", produtos)
+	defer db.Close()
 }
 
 func main() {
