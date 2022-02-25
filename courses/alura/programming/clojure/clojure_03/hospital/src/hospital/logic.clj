b6cf0fcc953e65140772e5d173a81356d43adec4
Gabriel Gian <gabrielgian@protonmail.com>
2022-02-25 20:04:53 -0300

    Add juxt for multiple function calls

--- courses/alura/programming/clojure/clojure_03/hospital/src/hospital/logic.clj
+++ courses/alura/programming/clojure/clojure_03/hospital/src/hospital/logic.clj
@@ -1,5 +1,4 @@
 (ns hospital.logic)
-
 (defn cabe-na-fila?
   [hospital departamento]
   (-> hospital
@@ -29,3 +28,30 @@
 (defn atende
   [hospital departamento]
   (update hospital departamento pop))
+
+(defn proxima
+  [hospital departamento]
+  (-> hospital
+      departamento
+      peek))
+
+(defn transfere
+  [hospital de para]
+  (let [pessoa (proxima hospital de)]
+    (-> hospital
+        (atende de)
+        (chega-em para pessoa))))
+
+(defn atende-completo
+  [hospital departamento]
+  {:paciente (update hospital departamento peek)
+   :fila (update hospital departamento pop)})
+
+(defn atende-completo-chama-ambos
+  [hospital departamento]
+  (let [fila (get hospital departamento)
+        peel-pop (juxt peek pop)
+        [pessoa fila-atualizada] (peek-pop fila)
+        hospital-atual (update hospital departamento fila-atualizada)]
+    {:paciente pessoa
+     :hospital hospital-atual}))
