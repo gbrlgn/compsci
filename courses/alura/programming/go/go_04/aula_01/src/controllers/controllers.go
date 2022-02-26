c41e8a7c9422c80ecd71608b9b0461b9b2bf4e94
Gabriel Gian <gabrielgian@protonmail.com>
2022-02-26 10:48:13 -0300

    Expose JSON API

--- courses/alura/programming/go/go_04/aula_01/src/controllers/controllers.go
+++ courses/alura/programming/go/go_04/aula_01/src/controllers/controllers.go
@@ -1,10 +1,16 @@
 package controllers
 
 import (
+	"alura/go-rest-api/models"
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Home Page")
+}
+
+func Personalidades(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(models.Personalidades)
 }
--- courses/alura/programming/go/go_04/aula_01/src/main.go
+++ courses/alura/programming/go/go_04/aula_01/src/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"alura/go-rest-api/models"
 	"alura/go-rest-api/routes"
 	"fmt"
 )
 
 func main() {
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Afonso Pena", Historia: "..."},
+		{Nome: "Adélia Prado", Historia: "..."},
+	}
+
 	fmt.Println("Iniciando o servidor REST")
 	routes.HandleRequest()
 }
