49cf57d919594aabe46bb566c69817b2c5961fd5
Gabriel Gian <gabrielgian@protonmail.com>
2022-02-27 10:50:18 -0300

    Add files from class 4

---
+++ courses/alura/programming/go/go_04/aula_03/src/controllers/controllers.go
@@ -0,0 +1,33 @@
+package controllers
+
+import (
+	"alura/go-rest-api/database"
+	"alura/go-rest-api/models"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Home Page")
+}
+
+func Personalidades(w http.ResponseWriter, r *http.Request) {
+	var p []models.Personalidade
+	database.DB.Find(&p)
+	json.NewEncoder(w).Encode(p)
+}
+
+func RetornarPersonalidade(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	for _, personalidade := range models.Personalidades {
+		if strconv.Itoa(personalidade.Id) == id {
+			json.NewEncoder(w).Encode((personalidade))
+		}
+	}
+}
