5d707cdc66a0deb7ceed0ad5d93d310104599987
Gabriel Gian <gabrielgian@protonmail.com>
2022-03-01 12:57:43 -0300

    Refactor producers and consumers

---
+++ courses/alura/devops/kafka/kafka_01/src/main/java/br/com/alura/ecommerce/EmailSrv.java
@@ -0,0 +1,48 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.UUID;
+
+public class EmailSrv {
+
+    public static void main(String[] args) {
+        var emailService = new EmailSrv();
+        var service = new KafkaService(EmailSrv.class.getSimpleName(),
+                "ECOMMERCE_SEND_EMAIL", emailService::parse);
+        service.run();
+    }
+
+    private void parse(ConsumerRecord<String, String> record) {
+        System.out.println("--------------");
+        System.out.println("Sending email.");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Email sent.");
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailSrv.class.getSimpleName());
+        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, FraudDetectorSrv.class.getSimpleName() + "-" + UUID.randomUUID().toString());
+        return properties;
+    }
+}
