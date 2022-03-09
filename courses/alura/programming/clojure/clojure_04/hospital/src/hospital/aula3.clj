865ea6168ae4a71ba2c02bcd6b74cfde1dde7dfb
Gabriel Gian <gabrielgian@protonmail.com>
2022-03-09 20:39:34 -0300

    Compartmentalize protocol and record

--- courses/alura/programming/clojure/clojure_04/hospital/src/hospital/aula3.clj
+++ courses/alura/programming/clojure/clojure_04/hospital/src/hospital/aula3.clj
@@ -1,5 +1,7 @@
 (ns hospital.aula3
-  (:use clojure.pprint))
+  (:use clojure.pprint)
+  (:require [hospital.logic :as h.logic
+             hospital.model :as h.model]))
 
 (defn carrega-paciente [id]
   (println "Carregando" id)
@@ -10,12 +12,29 @@
 (pptint (carrega-paciente 30))
 
 (defn carrega-se-nao-existe
-  [pacientes id]
-  (if (contains? pacientes id)
-    pacientes
-    (let [paciente (carrega-paciente id)]
-      (assoc pacientes id paciente))))
+  [cache id carregadora]
+  (if (contains? cache id)
+    cache
+    (let [item (carregadora id)]
+      (assoc cache id item))))
 
 (pprint (carrega-se-nao-existe {} 15))
 
 (pprint (carrega-se-nao-existe {15 {:id 15}} 15))
+
+(defprotocol Carregavel
+  (carrega! [this id]))
+
+(defrecord Cache
+    [cache carregadora]
+  Carregavel
+  (carrega! [this id]
+    (swap! cache carrega-se-nao-existe id carregadora)
+    (get @cache id)))
+
+(def pacientes (->Cache (atom {} carrega-paciente)))
+(pprint pacientes)
+(carrega! pacientes 15)
+(carrega! pacientes 30)
+(carrega! pacientes 15)
+(pprint pacientes)
