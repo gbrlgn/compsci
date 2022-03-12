e02e195193e0cfec248033c76d7b3c6514221b11
Gabriel Gian <gabrielgian@protonmail.com>
2022-03-12 10:53:16 -0300

    Add Meta and abstractmethod

--- courses/alura/programming/python/python_04/ABC.py
+++ courses/alura/programming/python/python_04/ABC.py
@@ -1,12 +1,24 @@
-from abc import ABC
+from abc import ABCMeta, abstractmethod
 from collections.abc import MutableSequence
 from numbers import Complex
+
 
 class Playlist(MutableSequence):
     pass
 
+
 filmes = Playlist()
+
 
 class Numero(Complex):
     def __getitem__(self, item):
         super().__getitem__()
+
+
+numero = Numero()
+
+
+class Programa(metaclass=ABCMeta):
+    @abstractmethod
+    def __str__(self):
+        pass
