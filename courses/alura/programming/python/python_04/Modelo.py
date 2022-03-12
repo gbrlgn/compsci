16395d27fbde43287729be0d9e2644e18c8af3e5
Gabriel Gian <gabrielgian@protonmail.com>
2022-03-12 10:17:20 -0300

    Create iterable Playlist class

--- courses/alura/programming/python/python_04/Modelo.py
+++ courses/alura/programming/python/python_04/Modelo.py
@@ -46,19 +46,37 @@ class Serie(Programa):
         return f'{self.nome} - {self.ano} - {self.temporadas} temporadas - {self.likes}'
 
 
+class Playlist(list):
+    def __init__(self, nome, programas):
+        self.nome = nome
+        # Objeto a ser iterado no lugar da classe.
+        super().__init__(programas)
+
+    def tamanho(self):
+        return len(self.programas)
+
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
 
-vingadores.dar_likes()
-vingadores.dar_likes()
-vingadores.dar_likes()
-atlanta.dar_likes()
-atlanta.dar_likes()
+vingadores.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+atlanta.dar_like()
+atlanta.dar_like()
+atlanta.dar_like()
 
-print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
-print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
-
-filmes_e_series = [vingadores, atlanta]
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
 for programa in filmes_e_series:
-    programa.imprime()
+    print(programa)
+
+print(f'Tamanho do playlist: {len(playlist_fim_de_semana)}')
+print(f'Tá ou não tá? {demolidor in playlist_fim_de_semana}')
