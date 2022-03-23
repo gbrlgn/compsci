0b26e4e21f6270a28681e63b10bf58d1c3ce0ea9
Gabriel Gian <gabrielgian@protonmail.com>
2022-03-23 19:21:56 -0300

    Remove unwanted directories for restructuring

--- courses/alura/programming/design_patterns/observer/loja/src/br/com/alura/loja/orcamento/situacao/Finalizado.java
+++
@@ -1,9 +0,0 @@
-package br.com.alura.loja.orcamento.situacao;
-
-import br.com.alura.loja.orcamento.Orcamento;
-
-import java.math.BigDecimal;
-
-public class Finalizado extends SituacaoOrcamento {
-
-}
