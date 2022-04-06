42720d4ad4d210e9b24589ac3023d158147f358b
Gabriel Gian <gabrielgian@protonmail.com>
2022-04-06 11:58:07 -0300

    Update Hangman game

--- courses/alura/programming/cpp/cpp_01/forca.cpp
+++ courses/alura/programming/cpp/cpp_01/forca.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
+#include <map>
 
 using namespace std;
 
 const string PALAVRA = "MELANCIA";
+map<char, bool> chutou;
 
 bool letra_existe(char chute) {
     for (char letra : PALAVRA) {
@@ -15,23 +17,26 @@ bool letra_existe(char chute) {
 }
 
 int main () {
-    cout << "********************************" << endl;
-    cout <<  "* Bem-vindo ao jogo da forca! *" << endl;
-    cout <<  "********************************" << endl;
+    cout << "*******************************" << endl;
+    cout << "* Bem-vindo ao jogo da forca! *" << endl;
+    cout << "*******************************" << endl;
 
     bool nao_acertou = true;
     bool nao_enforcou = true;
     
     while (nao_acertou && nao_enforcou) {
         for (char letra : PALAVRA) {
-            if (JA_CHUTOU letra) {
+            if (chutou[letra]) {
                 cout << letra << " ";
+            } else {
+                cout << "_ ";
             }
         }
 
         cout <<  "Entre seu chute:" << endl;
         char chute;
         cin >> chute;
+        chutou[chute] = true;
         cout <<  "O seu chute foi: " << chute << endl;
 
         if (letra_existe(chute)) {
